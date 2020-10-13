edc3d8dbb9e9a396f7e8bd6f01859c02e04afb4a
Mokksh-bhatt <71543314+Mokksh-bhatt@users.noreply.github.com>
2020-10-13 17:01:06 +0530

    Add files via upload

---
+++ Project_31_PLINKO-master/sketch.js
@@ -0,0 +1,77 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var ground;
+var groundimg;
+var score = 0;
+
+var particles = [];
+var plinkos = [];
+var divisions = [];
+
+var divisionHeight = 300;
+
+function setup() { 
+  createCanvas(480,800);
+  
+  back = createSprite(240, 798, 480, 30);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(240, 785, 480, 10);
+
+  for (var i = 0; i <= width; i = i + 80){
+    divisions.push(new Division(i, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  for (var j = 40; j <= width; j = j + 50){
+    plinkos.push(new Plinko(j, 75));
+  }
+  for (var j = 15; j <= width - 10; j = j + 50){
+    plinkos.push(new Plinko(j, 175));
+  }
+  for (var j = 40; j <= width; j = j + 50){
+    plinkos.push(new Plinko(j,275));
+  }
+  for (var j = 15; j <= width - 10; j = j + 50){
+    plinkos.push(new Plinko(j, 375));
+  }
+
+  if (plinkos.colliedGround) {
+    score++;
+
+  }
+
+}
+function draw() {
+  Engine.update(engine);
+  background(0);
+  noStroke();
+  textSize(35)
+  fill("white");
+  text("score  "+ score, width-300,50)
+  
+  if (frameCount % 60 === 0){
+    particles.push(new Particle(random(width/2-20, width/2+20), 10, 10));
+  }
+
+  ground.display();
+  
+  for (var k = 0; k < particles.length; k++){
+    particles[k].display();
+  }
+
+  for (var j = 0; j < plinkos.length; j++){
+    plinkos[j].display();
+  }
+
+  for (var i = 0; i < divisions.length; i++){
+    divisions[i].display();
+  }
+
+  drawSprites();
+}
+
